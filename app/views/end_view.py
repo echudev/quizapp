4467a992876dd8f459225497372d48e72379530f
Ezequiel <ezequiel.maranda@gmail.com>
2024-06-28 01:19:51 -0300

    back & logout buttons created

--- app/views/end_view.py
+++ app/views/end_view.py
@@ -9,11 +9,13 @@ class EndView(ttk.Frame):
                
 
         text_frame = ttk.Frame(self)
-        label = ttk.Label(text_frame, text=f'Felicitaciones! llegaste al final', font=("Arial", 20))
+        label = ttk.Label(text_frame, text=f'Felicitaciones!', font=("Arial", 20))
         label.pack(pady=20, side='left')
         label_name = ttk.Label(text_frame, textvariable=self.parent.user_controller.user_model.username, font=("Arial", 20), foreground="lightgreen")
         label_name.pack(pady=20, side='left')
         text_frame.pack(pady=20, padx=20)
+        label2 = ttk.Label(text_frame, text=f'Llegaste al final!', font=("Arial", 20))
+        label2.pack(pady=20)
         
         text_frame2 = ttk.Frame(self)
         points_label = ttk.Label(text_frame2, text=f'Hiciste', font=("Arial", 14))
@@ -25,22 +27,9 @@ class EndView(ttk.Frame):
         text_frame2.pack(pady=20, padx=20)
 
     
-        buttons_frame = ttk.Frame(self)
-        play_again_button = ttk.Button(buttons_frame, text="Jugar de nuevo", padding=(20, 10), command = lambda: print('jugad de nuevo'))
+        play_again_button = ttk.Button(self, text="Volver", padding=(20, 10), command = self.volver)
         play_again_button.pack(padx=5, side='left')
-        profile_button = ttk.Button(buttons_frame, text="Mi Perfil", padding=(20, 10), command = lambda: print('mi perfil'))
-        profile_button.pack(padx=5, side='left')
-        logout_button = ttk.Button(buttons_frame, text="Salir", padding=(20, 10), command = lambda: print('Salir'), bootstyle="warning")
-        logout_button.pack(padx=5, side='left')
-        buttons_frame.pack(pady=20, padx=20)
     
     
-    def volver_a_jugar(self):
+    def volver(self):
         self.parent.show_frame('IntroView')
-
-    def ver_perfil(self):
-        self.parent.show_frame('PerfilView')
-
-    def salir(self):
-        messagebox.showinfo("Hasta luego!", "Gracias por jugar!")
-        self.parent.show_frame('LoginView')
--- app/views/intro_view.py
+++ app/views/intro_view.py
@@ -11,13 +11,26 @@ class IntroView(ttk.Frame):
         self.username_display = ttk.Label(self, textvariable=self.parent.user_controller.user_model.username, font=("Arial", 20), foreground="lightgreen")
         self.username_display.pack(pady=5)
 
-        intro_label = ttk.Label(self, text="Cuando estes listo presiona Start", font=("Arial", 14))
-        intro_label.pack(pady=2, padx=20)
-        intro_label2 = ttk.Label(self, text="Y empezá a responder!", font=("Arial", 14))
-        intro_label2.pack(pady=2, padx=20)
+        intro_label = ttk.Label(self, text="Juego de preguntas y respuestas", font=("Arial", 14))
+        intro_label.pack(padx=20)
+        intro_label2 = ttk.Label(self, text="sobre el lenguaje Python", font=("Arial", 14))
+        intro_label2.pack(padx=5)
     
-        start_button = ttk.Button(self, text="Start", padding=(20, 10), command = self.start_game)
-        start_button.pack(pady=20)
+        start_button = ttk.Button(self, text="Jugar", padding=(20, 10), command = self.start_game)
+        start_button.pack(pady=10)
+
+        start_button = ttk.Button(self, text="Perfil", padding=(20, 10), command = self.goto_profile)
+        start_button.pack(pady=10)
+
+        exit_button = ttk.Button(self, text="Salir", padding=(20, 10), command = self.salir)
+        exit_button.pack(pady=10)
     
     def start_game(self):
         self.parent.show_frame('GameView')
+
+    def goto_profile(self):
+        self.parent.show_frame('ProfileView')
+
+    def salir(self):
+        self.parent.user_controller.logout()
+        self.parent.show_frame('LoginView')

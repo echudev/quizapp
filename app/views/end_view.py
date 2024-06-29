e10ab342a3846779ffc6db546e812ea6328d18df
Ezequiel <ezequiel.maranda@gmail.com>
2024-06-29 04:53:42 -0300

    endview stlyes fixed

--- app/views/end_view.py
+++ app/views/end_view.py
@@ -1,6 +1,4 @@
 from tkinter import ttk
-from tkinter import messagebox
-
 
 class EndView(ttk.Frame):
     def __init__(self, parent):
@@ -10,20 +8,20 @@ class EndView(ttk.Frame):
 
         text_frame = ttk.Frame(self)
         label = ttk.Label(text_frame, text=f'Felicitaciones!', font=("Arial", 20))
-        label.pack(pady=20, side='left')
+        label.pack(pady=5, side='left')
         label_name = ttk.Label(text_frame, textvariable=self.parent.user_controller.user_model.username, font=("Arial", 20), foreground="lightgreen")
-        label_name.pack(pady=20, side='left')
-        text_frame.pack(pady=20, padx=20)
+        label_name.pack(pady=5, side='left')
+        text_frame.pack(padx=20)
 
-        label2 = ttk.Label(text_frame, text=f'Llegaste al final!', font=("Arial", 20))
-        label2.pack(pady=20, side='left')
+        label2 = ttk.Label(self, text=f'Llegaste al final!', font=("Arial", 16))
+        label2.pack(pady=10)
         
         text_frame2 = ttk.Frame(self)
-        points_label = ttk.Label(text_frame2, text=f'Hiciste', font=("Arial", 14))
+        points_label = ttk.Label(text_frame2, text=f'Hiciste', font=("Arial", 20))
         points_label.pack(pady=20, side='left')
         points_label2 = ttk.Label(text_frame2, textvariable=self.parent.game_controller.model.puntaje, font=("Arial", 20), foreground="lightgreen")
         points_label2.pack(pady=20, side='left')
-        points_label3 = ttk.Label(text_frame2, text=f'puntos.', font=("Arial", 14))
+        points_label3 = ttk.Label(text_frame2, text=f'puntos.', font=("Arial", 20))
         points_label3.pack(pady=20, side='left')
         text_frame2.pack(pady=20, padx=20)
 
